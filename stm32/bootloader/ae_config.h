e559491a31bc1710a41285c5e04ccabfec9622db
Peter D. Gray <peter@conalgo.com>
2018-12-06 13:43:43 -0500

    Document chip config better

--- stm32/bootloader/ae_config.h
+++ stm32/bootloader/ae_config.h
@@ -36,3 +36,75 @@
 #define KEYNUM_secret_4            	12
 #define KEYNUM_brickme             	13
 #define KEYNUM_firmware            	14
+
+/*
+
+RevNum: 00005000  len=4
+I2C_Enable = 0x0
+GPIO Mode = 0x1
+GPIO Default = 0x0
+GPIO Detect (vs authout) = 0x0
+GPIO SignalKey/KeyId = 0xe
+I2C_Address(sic) = 0xe1
+OTPmode = 0x55
+ChipMode = 0x0
+
+     Slot[0] = 0x0000 = SlotConfig(ReadKey=0, NoMac=0, LimitedUse=0, EncryptRead=0, IsSecret=0, WriteKey=0, WriteConfig=0)=0x0000
+KeyConfig[0] = 0x3c00 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=1, ReqRandom=0, ReqAuth=0, AuthKey=0, IntrusionDisable=0, RFU=0, X509id=0)=0x003c
+
+     Slot[1] = 0x8f2d = SlotConfig(ReadKey=15, NoMac=0, LimitedUse=0, EncryptRead=0, IsSecret=1, WriteKey=13, WriteConfig=2)=0x2d8f
+KeyConfig[1] = 0x5c00 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=0, ReqRandom=1, ReqAuth=0, AuthKey=0, IntrusionDisable=0, RFU=0, X509id=0)=0x005c
+
+     Slot[2] = 0x8f80 = SlotConfig(ReadKey=15, NoMac=0, LimitedUse=0, EncryptRead=0, IsSecret=1, WriteKey=0, WriteConfig=8)=0x808f
+KeyConfig[2] = 0xbc01 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=1, ReqRandom=0, ReqAuth=1, AuthKey=1, IntrusionDisable=0, RFU=0, X509id=0)=0x01bc
+
+     Slot[3] = 0x8f43 = SlotConfig(ReadKey=15, NoMac=0, LimitedUse=0, EncryptRead=0, IsSecret=1, WriteKey=3, WriteConfig=4)=0x438f
+KeyConfig[3] = 0xfc01 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=1, ReqRandom=1, ReqAuth=1, AuthKey=1, IntrusionDisable=0, RFU=0, X509id=0)=0x01fc
+
+     Slot[4] = 0x8f44 = SlotConfig(ReadKey=15, NoMac=0, LimitedUse=0, EncryptRead=0, IsSecret=1, WriteKey=4, WriteConfig=4)=0x448f
+KeyConfig[4] = 0xfc01 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=1, ReqRandom=1, ReqAuth=1, AuthKey=1, IntrusionDisable=0, RFU=0, X509id=0)=0x01fc
+
+     Slot[5] = 0x0043 = SlotConfig(ReadKey=0, NoMac=0, LimitedUse=0, EncryptRead=0, IsSecret=0, WriteKey=3, WriteConfig=4)=0x4300
+KeyConfig[5] = 0x9c01 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=0, ReqRandom=0, ReqAuth=1, AuthKey=1, IntrusionDisable=0, RFU=0, X509id=0)=0x019c
+
+     Slot[6] = 0x0044 = SlotConfig(ReadKey=0, NoMac=0, LimitedUse=0, EncryptRead=0, IsSecret=0, WriteKey=4, WriteConfig=4)=0x4400
+KeyConfig[6] = 0x9c01 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=0, ReqRandom=0, ReqAuth=1, AuthKey=1, IntrusionDisable=0, RFU=0, X509id=0)=0x019c
+
+     Slot[7] = 0x8f47 = SlotConfig(ReadKey=15, NoMac=0, LimitedUse=0, EncryptRead=0, IsSecret=1, WriteKey=7, WriteConfig=4)=0x478f
+KeyConfig[7] = 0xfc01 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=1, ReqRandom=1, ReqAuth=1, AuthKey=1, IntrusionDisable=0, RFU=0, X509id=0)=0x01fc
+
+     Slot[8] = 0x8f48 = SlotConfig(ReadKey=15, NoMac=0, LimitedUse=0, EncryptRead=0, IsSecret=1, WriteKey=8, WriteConfig=4)=0x488f
+KeyConfig[8] = 0xfc01 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=1, ReqRandom=1, ReqAuth=1, AuthKey=1, IntrusionDisable=0, RFU=0, X509id=0)=0x01fc
+
+     Slot[9] = 0xc343 = SlotConfig(ReadKey=3, NoMac=0, LimitedUse=0, EncryptRead=1, IsSecret=1, WriteKey=3, WriteConfig=4)=0x43c3
+KeyConfig[9] = 0xdc03 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=0, ReqRandom=1, ReqAuth=1, AuthKey=3, IntrusionDisable=0, RFU=0, X509id=0)=0x03dc
+
+     Slot[10] = 0xc444 = SlotConfig(ReadKey=4, NoMac=0, LimitedUse=0, EncryptRead=1, IsSecret=1, WriteKey=4, WriteConfig=4)=0x44c4
+KeyConfig[10] = 0xdc04 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=0, ReqRandom=1, ReqAuth=1, AuthKey=4, IntrusionDisable=0, RFU=0, X509id=0)=0x04dc
+
+     Slot[11] = 0xc747 = SlotConfig(ReadKey=7, NoMac=0, LimitedUse=0, EncryptRead=1, IsSecret=1, WriteKey=7, WriteConfig=4)=0x47c7
+KeyConfig[11] = 0xdc07 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=0, ReqRandom=1, ReqAuth=1, AuthKey=7, IntrusionDisable=0, RFU=0, X509id=0)=0x07dc
+
+     Slot[12] = 0xc848 = SlotConfig(ReadKey=8, NoMac=0, LimitedUse=0, EncryptRead=1, IsSecret=1, WriteKey=8, WriteConfig=4)=0x48c8
+KeyConfig[12] = 0xdc08 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=0, ReqRandom=1, ReqAuth=1, AuthKey=8, IntrusionDisable=0, RFU=0, X509id=0)=0x08dc
+
+     Slot[13] = 0x8f4d = SlotConfig(ReadKey=15, NoMac=0, LimitedUse=0, EncryptRead=0, IsSecret=1, WriteKey=13, WriteConfig=4)=0x4d8f
+KeyConfig[13] = 0xfc01 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=1, ReqRandom=1, ReqAuth=1, AuthKey=1, IntrusionDisable=0, RFU=0, X509id=0)=0x01fc
+
+     Slot[14] = 0x8f43 = SlotConfig(ReadKey=15, NoMac=0, LimitedUse=0, EncryptRead=0, IsSecret=1, WriteKey=3, WriteConfig=4)=0x438f
+KeyConfig[14] = 0xdc01 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=0, ReqRandom=1, ReqAuth=1, AuthKey=1, IntrusionDisable=0, RFU=0, X509id=0)=0x01dc
+
+     Slot[15] = 0x0000 = SlotConfig(ReadKey=0, NoMac=0, LimitedUse=0, EncryptRead=0, IsSecret=0, WriteKey=0, WriteConfig=0)=0x0000
+KeyConfig[15] = 0x3c00 = KeyConfig(Private=0, PubInfo=0, KeyType=7, Lockable=1, ReqRandom=0, ReqAuth=0, AuthKey=0, IntrusionDisable=0, RFU=0, X509id=0)=0x003c
+
+Counter[0]: ffffffff00000000  len=8
+Counter[1]: ffffffff00000000  len=8
+LastKeyUse: ffffffffffffffffffffffffffffffff  len=16
+UserExtra = 0x0
+Selector = 0x0
+LockValue = 0x55
+LockConfig = 0x55
+SlotLocked: ffff  len=2
+X509format: 00000000  len=4
+
+*/
