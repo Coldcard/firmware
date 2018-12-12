8718dfb858c5c8cf0cb47d575a3f61ea2abb8e4f
Peter D. Gray <peter@conalgo.com>
2018-12-12 11:26:27 -0500

    passing bad_logins

--- testing/test_pincodes.py
+++ testing/test_pincodes.py
@@ -8,6 +8,7 @@
 # - will erase seed
 # - assumes no PIN set yet
 # - dev mode must be enabled
+# - these tests need to run individually, not working well all together
 #
 import time, pytest, os
 from ckcc_protocol.protocol import CCProtocolPacker, CCProtoError, MAX_TXN_LEN, CCUserRefused
@@ -69,8 +70,11 @@ def test_prefix_words(repl, setup_repl):
     assert a1 == a3
 
 def test_greenlight(repl, setup_repl):
+    from random import randint
+
+    # NOTE: the return values and names of these functions are all stupid.
+
     assert repl.eval("pa.greenlight_firmware()", max_time=5) == None
-    assert repl.eval("dis.clear(); dis.text(0,0, 'okay'); dis.show()") == 28
 
     assert repl.eval("callgate.get_genuine()") == 1
     assert repl.eval("callgate.clear_genuine()") == None
@@ -79,7 +83,77 @@ def test_greenlight(repl, setup_repl):
     assert repl.eval("callgate.set_genuine()", max_time=5) == 0
     assert repl.eval("callgate.get_genuine()") == 1
 
+    # this changes flash.
+    assert repl.eval("open('/flash/test', 'wb').write(b'hi %06d')" % randint(1,1e6)) >= 3
+
+    # 'set_genuine' really means "test if genuine" here
+    assert repl.eval("callgate.set_genuine()", max_time=5) == -1
+    assert repl.eval("callgate.get_genuine()") == 1
+
+    assert repl.eval("pa.greenlight_firmware()", max_time=5) == None
+
+    assert repl.eval("callgate.set_genuine()", max_time=5) == 0
+    assert repl.eval("callgate.get_genuine()") == 1
+
     assert repl.eval("dis.clear(); dis.text(0,0, 'done'); dis.show()") == 28
 
+@pytest.mark.parametrize('is_sec', [ False, True])
+def test_duress(repl, setup_repl, is_sec):
+    assert repl.eval("pa.setup(b'', secondary=%r)" % is_sec) == 3
+
+    assert repl.eval('pa.has_duress_pin()') == False
+    assert repl.eval('pa.is_successful()') == True
+    assert repl.eval("pa.change(is_duress=True, new_pin=b'34-34', old_pin=b'')") == None
+    assert repl.eval("pa.change(is_duress=True, new_secret=b'a'*72, old_pin=b'34-34')") == None
+    assert repl.eval("pa.fetch(duress_pin=b'34-34')") == b'a'*72
+    assert repl.eval("pa.change(is_duress=True, new_secret=bytes(72), old_pin=b'34-34', new_pin=b'')") == None
+    assert repl.eval('pa.has_duress_pin()') == False
+
+    # cleanup
+    if is_sec:
+        repl.eval("pa.setup(b'')")
+
+@pytest.mark.parametrize('is_sec', [ False, True])
+@pytest.mark.parametrize('nfails', [1, 3])
+def test_bad_logins(repl, setup_repl, is_sec, nfails):
+    if repl.eval("pa.setup(b'', secondary=%r)" % is_sec) != 3:
+        # robustness
+        repl.eval("pa.setup(b'12-12', secondary=%r)" % is_sec)
+        repl.eval("[pa.delay() for i in range(pa.delay_required)]")
+        assert repl.eval("pa.login()") == True
+
+    assert repl.eval("pa.change(new_pin=b'12-12')") == None
+    assert repl.eval("pa.setup(b'12-12', secondary=%r)" % is_sec) == 0
+    assert repl.eval("pa.login()") == True
+
+    def prepare_attempt(pin):
+        assert repl.eval("pa.setup(%r, secondary=%r)" % (pin, is_sec)) == 0
+        nf = repl.eval('pa.num_fails')
+        nd = repl.eval('pa.delay_required')
+        if nf: assert nd >= 1
+        for x in range(nd):
+            repl.eval('pa.delay()')
+        assert repl.eval('pa.delay_required') == repl.eval('pa.delay_achieved')
+        return nf
+
+    # try wrong pin a few times
+    for n in range(nfails):
+        nf = prepare_attempt(b'xx')
+
+        assert nf == n
+
+        with pytest.raises(RuntimeError) as ee:
+            repl.eval("pa.login()")
+        assert 'AUTH_FAIL' in ee.value.args[0]
+        
+    # should be successful now
+    prepare_attempt(b'12-12')
+    assert repl.eval("pa.login()") == True
+
+    # reset state
+    assert repl.eval("pa.change(new_pin=b'')") == None
+    assert repl.eval("pa.setup(b'', secondary=%r)" % is_sec) == 3
+    assert repl.eval("pa.delay_required") == 0
+    assert repl.eval("pa.num_fails") == 0
 
 # EOF
