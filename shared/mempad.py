2631b12d56dc88a828866d8d67b13d38b64010a1
Peter D. Gray <peter@conalgo.com>
2019-04-02 14:33:59 -0400

    Cleanup and refining debounce for membrane keypad

--- shared/mempad.py
+++ shared/mempad.py
@@ -10,11 +10,13 @@ from random import shuffle
 from numpad import NumpadBase
 
 NUM_ROWS = const(4)
-HISTORY_LEN = const(3)
-SAMPLE_RATE = const(10)        # ms
-NUM_SAMPLES = const(NUM_ROWS * HISTORY_LEN)
+NUM_COLS = const(3)
+SAMPLE_RATE = const(5)          # ms
+NUM_SAMPLES = const(10)         # this many matching samples required for debounce
 
 class MembraneNumpad(NumpadBase):
+    # (row, col) => keycode
+    DECODER = 'y0x987654321'
 
     def __init__(self, loop):
         super(MembraneNumpad, self).__init__(loop)
@@ -30,15 +32,15 @@ class MembraneNumpad(NumpadBase):
         self.rows = [Pin(i, Pin.OUT_OD, value=0) 
                         for i in ('M2_ROW0', 'M2_ROW1', 'M2_ROW2', 'M2_ROW3')]
 
-        self.scan_order = array.array('b', list(range(NUM_ROWS)) * HISTORY_LEN)
-        self.history = array.array('b', (-1 for i in range(NUM_SAMPLES)))
+        self.scan_order = array.array('b', list(range(NUM_ROWS)))
+        self.history = bytearray(NUM_ROWS * NUM_COLS)
 
         # We scan in random order, because Tempest.
         # - scanning only starts when something pressed
         # - complete scan is done before acting on what was measured
         shuffle(self.scan_order)
 
-        self.scan_idx = 0
+        self.scan_count = 0
         self.waiting_for_any = True
 
         for c in self.cols:
@@ -72,83 +74,84 @@ class MembraneNumpad(NumpadBase):
 
         self.waiting_for_any = False
 
-        # First irq may be a runt of unknown length, so don't collect data 
-        # until after first time called.
-        self.scan_idx = NUM_SAMPLES
+        assert self.scan_count == 0
+        self.scan_count = NUM_SAMPLES-1
+        self.history = bytearray(NUM_ROWS * NUM_COLS)
         self.timer.init(freq=1000//SAMPLE_RATE, callback=self._measure_irq)
         self.loop.call_later_ms(SAMPLE_RATE * (NUM_SAMPLES + 2), self._finish_scan)
 
-    def _scan_next(self):
-        # enable detection on one row
-        active_row = self.scan_order[self.scan_idx]
-
-        # unrolled because called during irq, no memory alloc
-        self.rows[0].value(active_row != 0)
-        self.rows[1].value(active_row != 1)
-        self.rows[2].value(active_row != 2)
-        self.rows[3].value(active_row != 3)
-
     def _measure_irq(self, _timer):
-        # CAUTION: Called at high rate, and cannot do memory alloc.
+        # CHALLENGE: Called at high rate, and cannot do memory alloc.
+        # - sample all keys once, record any that are pressed
 
-        # sample the column values
-        if self.cols[0].value() == 0:
-            col_press = 0
-        elif self.cols[1].value() == 0:
-            col_press = 1
-        elif self.cols[2].value() == 0:
-            col_press = 2
+        if self.scan_count == NUM_SAMPLES:
+            # First irq may be a runt of unknown length, so don't collect data 
+            # until after first time called.
+            pass
         else:
-            col_press = -1
+            for i in range(NUM_ROWS):
+                row = self.scan_order[i]
 
-        if self.scan_idx != NUM_SAMPLES:
-            # track sample data
-            self.history[self.scan_idx] = col_press
+                self.rows[0].value(row != 0)
+                self.rows[1].value(row != 1)
+                self.rows[2].value(row != 2)
+                self.rows[3].value(row != 3)
 
-        # move to next column, or stop
-        if self.scan_idx == 0:
+                # sample the column values
+                if self.cols[0].value() == 0:
+                    col = 0
+                elif self.cols[1].value() == 0:
+                    col = 1
+                elif self.cols[2].value() == 0:
+                    col = 2
+                else:
+                    continue
+
+                # track any press observed
+                self.history[(row * NUM_COLS) + col] += 1
+
+        # should we do that again in a bit?
+        if self.scan_count == 0:
             # we are done a full scan
             _timer.deinit()
         else:
-            self.scan_idx -= 1
-            self._scan_next()
+            self.scan_count -= 1
 
     def _finish_scan(self):
-        # we're done a full scan (mulitple times: HISTORY_LEN)
-        score = {}
+        # we're done a full scan (mulitple times: NUM_SAMPLES)
+        # - not trying to support multiple presses, just one
+        from main import dis
+        from display import FontTiny
 
-        for i, col_press in enumerate(self.history):
-            if col_press == -1:
-                # nothing pressed in this row
-                continue
+        assert self.scan_count == 0
 
-            key = self.DECODER[(self.scan_order[i], col_press)]
-
-            if key not in score:
-                score[key] = 1
-            else:
-                score[key] += 1
-
-        if not score:
+        if sum(self.history) == 0:
             # all keys are 100% up.
             self._key_event('')
 
             # stop scanning for now
             self._wait_any()
+            #print('=> all up')
+            return
 
-        else:
-            # handle debounce, which happens in both directions: press and release
-            # - all samples must be in agreement to count as either
+        #print(' '.join(str(i) for i in self.history), end='')
 
-            for key, count in score.items():
-                if count == HISTORY_LEN:
-                    self._key_event(key)
-                    break
+        # handle debounce, which happens in both directions: press and release
+        # - all samples must be in agreement to count as either
+        for rc, count in enumerate(self.history):
+            key = self.DECODER[rc]
 
-            # last key was fully released
-            if self.key_pressed not in score:
+            if count == 0 and key == self.key_pressed:
+                # key up
                 self._key_event('')
+                #print(' => %s UP' % key)
+                break
+            elif count == NUM_SAMPLES:
+                self._key_event(key)
+                #print(' => %s down' % key)
+                break
 
-            self._start_scan()
+        # do another scan
+        self._start_scan()
     
 # EOF
