221bfb538978d84d6318a89536f5ff038e5aca15
Peter D. Gray <peter@conalgo.com>
2019-10-07 11:29:44 -0400

    Fixes, verify low-level values match UX values

--- testing/test_change_pins.py
+++ testing/test_change_pins.py
@@ -3,11 +3,45 @@
 #
 # UX and interactions related to Settings > Pin Options
 #
+# - altho this can pass on the simulator, it's much more interesting to test with "bare metal"
+# - need this to recover:
+#       from main import pa; pa.change(is_duress=1, new_pin=b'', old_pin=b'77-77')
+# - same tests are needed in three modes: normal login, secondary PIN used for login, and
+#   duress PIN used for login.
+#
 import pytest, time, os
 from helpers import xfp2str
 
 DEF_PIN = '12-12'
 CLR_PIN = '999999-999999'
+
+@pytest.fixture
+def get_duress_secret(sim_eval):
+    def doit(pin):
+        # read the duress secret
+        rv = sim_eval(f'main.pa.fetch(duress_pin=b"{pin}")', timeout=9000)
+        if rv.startswith('Traceback'):
+            raise RuntimeError(rv)
+        assert rv[0:2] == "b'"
+        return eval(rv)
+    return doit
+
+@pytest.fixture
+def verify_pin_set(sim_eval):
+    def doit(pin, secondary=False, duress=False, brickme=False):
+        # check the SE holds the indicated PIN code
+        kws = ''
+        if secondary:
+            kws += ", is_secondary=1"
+        if duress:      # not used
+            kws += ", is_duress=1"
+        if brickme:
+            kws += ", is_brickme=1"
+        rv = sim_eval(f'main.pa.change(new_pin=b"{pin}", old_pin=b"{pin}" {kws})', timeout=9000)
+        if rv != 'None':
+            raise RuntimeError(rv)
+
+    return doit
 
 @pytest.fixture
 def goto_pin_options(pick_menu_item, goto_home):
@@ -27,7 +61,9 @@ def enter_pin(cap_screen, need_keypress):
         assert scr[2] == 'Enter PIN Prefix'
         for ch in pin:
             if ch != '-':
+                time.sleep(.05)      # required?
                 need_keypress(ch)
+                time.sleep(.05)      # required?
                 continue
 
             if ch == '-':
@@ -54,11 +90,15 @@ def enter_pin(cap_screen, need_keypress):
 @pytest.fixture
 def change_pin(cap_screen, cap_story, cap_menu, need_keypress, enter_pin):
     def doit(old_pin, new_pin, hdr_text):
+        # use standard menus and UX to change a PIN 
         title, story = cap_story()
         assert title == hdr_text
         assert ('will be changing the' in story) or (CLR_PIN in story)
         need_keypress('y')
         time.sleep(0.01)      # required
+
+        assert max(len(i) for i in new_pin.split('-')) <= 6
+        assert 2 <= min(len(i) for i in new_pin.split('-'))
 
         # give old pin, if there was one
         if old_pin != None:
@@ -87,30 +127,45 @@ def change_pin(cap_screen, cap_story, cap_menu, need_keypress, enter_pin):
 
     return doit
 
-@pytest.mark.parametrize('new_pin', ['77-77', '123456-654321', '79-65454321', '123456-12'])
-def test_main_pin(goto_pin_options, pick_menu_item, cap_story, cap_screen, need_keypress, change_pin, new_pin):
+@pytest.mark.parametrize('new_pin', ['77-77', '123456-654321', '79-654321', '123456-12'])
+def test_main_pin(goto_pin_options, pick_menu_item, cap_story, cap_screen, need_keypress, change_pin, new_pin, verify_pin_set):
     goto_pin_options()
 
     pick_menu_item('Change Main PIN')
     change_pin(DEF_PIN, new_pin, 'Main PIN')
+    verify_pin_set(new_pin)
 
     pick_menu_item('Change Main PIN')
     change_pin(new_pin, DEF_PIN, 'Main PIN')
+    verify_pin_set(DEF_PIN)
 
-@pytest.mark.parametrize('new_pin', ['77-77', '123456-654321', '79-65454321', '123456-12'])
-def test_duress_pin(goto_pin_options, pick_menu_item, cap_story, cap_screen, need_keypress, change_pin, new_pin):
+@pytest.mark.parametrize('new_pin', ['77-77', '123456-654321', '79-654321', '123456-12'])
+def test_duress_pin(goto_pin_options, pick_menu_item, cap_story, cap_screen, need_keypress, change_pin, new_pin, get_duress_secret):
     goto_pin_options()
+
     pick_menu_item('Duress PIN')
     change_pin(None, new_pin, 'Duress PIN')
 
+    # duress secret should be complex
+    d_secret = get_duress_secret(new_pin)
+    assert len(set(d_secret)) > 20
+    assert d_secret[0] == 0x01, "not xprv?"
+
+    # changing PIN shouldn't change secret
     pick_menu_item('Duress PIN')
     change_pin(new_pin, '123-123', 'Duress PIN')
+    rb = get_duress_secret('123-123')
+    assert rb == d_secret
 
     pick_menu_item('Duress PIN')
     change_pin('123-123', CLR_PIN, 'Duress PIN')
 
-@pytest.mark.parametrize('new_pin', ['77-77', '123456-654321', '79-65454321', '123456-12'])
-def test_secondary_pin(is_mark3, goto_pin_options, pick_menu_item, cap_story, cap_screen, need_keypress, change_pin, new_pin):
+    # clearing PIN should clear secret
+    zz = get_duress_secret('')
+    assert zz == b'\0'*72
+
+@pytest.mark.parametrize('new_pin', ['77-77', '123456-654321', '79-654321', '123456-12'])
+def test_secondary_pin(is_mark3, goto_pin_options, pick_menu_item, cap_story, cap_screen, need_keypress, change_pin, new_pin, verify_pin_set):
 
     goto_pin_options()
 
@@ -119,19 +174,23 @@ def test_secondary_pin(is_mark3, goto_pin_options, pick_menu_item, cap_story, ca
 
     pick_menu_item('Second Wallet')
     change_pin(None, new_pin, 'Second PIN')
+    verify_pin_set(new_pin, secondary=1)
 
     pick_menu_item('Second Wallet')
     change_pin(new_pin, CLR_PIN, 'Second PIN')
+    verify_pin_set('', secondary=1)
 
-@pytest.mark.parametrize('new_pin', ['77-77', '123456-654321', '79-65454321', '123456-12'])
-def test_brickme_pin(goto_pin_options, pick_menu_item, cap_story, cap_screen, need_keypress, change_pin, new_pin):
+@pytest.mark.parametrize('new_pin', ['77-77', '123456-654321', '79-654321', '123456-12'])
+def test_brickme_pin(goto_pin_options, pick_menu_item, cap_story, cap_screen, need_keypress, change_pin, new_pin, verify_pin_set):
 
     goto_pin_options()
 
     pick_menu_item('Brick Me PIN')
     change_pin(None, new_pin, 'Brickme PIN')
+    verify_pin_set(new_pin, brickme=1)
 
     pick_menu_item('Brick Me PIN')
     change_pin(new_pin, CLR_PIN, 'Brickme PIN')
+    verify_pin_set('', brickme=1)
 
 # EOF
