7970888f020f1382c6e4a51e1d41021bab81f474
Peter D. Gray <peter@conalgo.com>
2020-01-09 23:22:04 -0500

    Pared down

--- shared/hmac.py
+++ shared/hmac.py
@@ -2,26 +2,12 @@
 
 Implements the HMAC algorithm as described by RFC 2104.
 
-from: https://github.com/micropython/micropython-lib/blob/master/hmac/hmac.py @ 96c981b1ee95ab391d55a83fead1d50107814320
+from: https://github.com/micropython/micropython-lib/blob/master/hmac/hmac.py @ 96c981b
 """
 
-import warnings as _warnings
-#from _operator import _compare_digest as compare_digest
-import hashlib as _hashlib
-PendingDeprecationWarning = None
-RuntimeWarning = None
-
-trans_5C = bytes((x ^ 0x5C) for x in range(256))
-trans_36 = bytes((x ^ 0x36) for x in range(256))
 
 def translate(d, t):
     return bytes(t[x] for x in d)
-
-# The size of the digests returned by HMAC depends on the underlying
-# hashing module used.  Use digest_size from the instance of HMAC instead.
-digest_size = None
-
-
 
 class HMAC:
     """RFC 2104 HMAC class.  Also complies with RFC 4231.
@@ -37,10 +23,6 @@ class HMAC:
         msg:       Initial input for the hash, if provided.
         digestmod: A module supporting PEP 247.  *OR*
                    A hashlib constructor returning a new hash object. *OR*
-                   A hash name suitable for hashlib.new().
-                   Defaults to hashlib.md5.
-                   Implicit default to hashlib.md5 is deprecated and will be
-                   removed in Python 3.6.
 
         Note: key and msg must be a bytes or bytearray objects.
         """
@@ -48,17 +30,8 @@ class HMAC:
         if not isinstance(key, (bytes, bytearray)):
             raise TypeError("key: expected bytes or bytearray, but got %r" % type(key).__name__)
 
-        if digestmod is None:
-            _warnings.warn("HMAC() without an explicit digestmod argument "
-                           "is deprecated.", PendingDeprecationWarning, 2)
-            digestmod = _hashlib.md5
-
-        if callable(digestmod):
-            self.digest_cons = digestmod
-        elif isinstance(digestmod, str):
-            self.digest_cons = lambda d=b'': _hashlib.new(digestmod, d)
-        else:
-            self.digest_cons = lambda d=b'': digestmod.new(d)
+        assert callable(digestmod)
+        self.digest_cons = digestmod
 
         self.outer = self.digest_cons()
         self.inner = self.digest_cons()
@@ -66,15 +39,16 @@ class HMAC:
 
         if hasattr(self.inner, 'block_size'):
             blocksize = self.inner.block_size
-            if blocksize < 16:
-                _warnings.warn('block_size of %d seems too small; using our '
-                               'default of %d.' % (blocksize, self.blocksize),
-                               RuntimeWarning, 2)
-                blocksize = self.blocksize
+            assert blocksize >= 16
+            #if blocksize < 16:
+            #    _warnings.warn('block_size of %d seems too small; using our '
+            #                   'default of %d.' % (blocksize, self.blocksize),
+            #                   RuntimeWarning, 2)
+            #    blocksize = self.blocksize
         else:
-            _warnings.warn('No block_size attribute on given digest object; '
-                           'Assuming %d.' % (self.blocksize),
-                           RuntimeWarning, 2)
+            #_warnings.warn('No block_size attribute on given digest object; '
+            #               'Assuming %d.' % (self.blocksize),
+            #               RuntimeWarning, 2)
             blocksize = self.blocksize
 
         # self.blocksize is the default blocksize. self.block_size is
@@ -84,58 +58,39 @@ class HMAC:
         if len(key) > blocksize:
             key = self.digest_cons(key).digest()
 
+        trans_5C = bytes((x ^ 0x5C) for x in range(256))
+        trans_36 = bytes((x ^ 0x36) for x in range(256))
+
         key = key + bytes(blocksize - len(key))
         self.outer.update(translate(key, trans_5C))
         self.inner.update(translate(key, trans_36))
+
         if msg is not None:
             self.update(msg)
-
-    @property
-    def name(self):
-        return "hmac-" + self.inner.name
 
     def update(self, msg):
         """Update this hashing object with the string msg.
         """
         self.inner.update(msg)
 
-    def copy(self):
-        """Return a separate copy of this hashing object.
-
-        An update to this copy won't affect the original object.
-        """
-        # Call __new__ directly to avoid the expensive __init__.
-        other = self.__class__.__new__(self.__class__)
-        other.digest_cons = self.digest_cons
-        other.digest_size = self.digest_size
-        other.inner = self.inner.copy()
-        other.outer = self.outer.copy()
-        return other
-
     def _current(self):
         """Return a hash object for the current state.
 
         To be used only internally with digest() and hexdigest().
         """
-        h = self.outer.copy()
+        h = self.outer
         h.update(self.inner.digest())
+        del self.outer
+        del self.inner
         return h
 
     def digest(self):
         """Return the hash value of this hashing object.
 
-        This returns a string containing 8-bit data.  The object is
-        not altered in any way by this function; you can continue
-        updating the object after calling this function.
+        This returns a string containing 8-bit data.
+        Single use only!
         """
-        h = self._current()
-        return h.digest()
-
-    def hexdigest(self):
-        """Like digest(), but returns a string of hexadecimal digits instead.
-        """
-        h = self._current()
-        return h.hexdigest()
+        return self._current().digest()
 
 def new(key, msg = None, digestmod = None):
     """Create a new hashing object and return it.
@@ -149,3 +104,8 @@ def new(key, msg = None, digestmod = None):
     method.
     """
     return HMAC(key, msg, digestmod)
+
+# Useful ones for this project
+#import tcc
+#hmac_sha256 = lambda key, msg=None: HMAC(key, msg, tcc.sha256)
+#hmac_sha1 = lambda key, msg=None: HMAC(key, msg, tcc.sha1)
