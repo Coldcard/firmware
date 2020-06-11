d151063f97dbc8f30bf1eb1545ceec319ddff1d4
Peter D. Gray <peter@conalgo.com>
2020-06-11 09:50:33 -0400

    Encrypt amount held in UTXOs we know about

--- shared/history.py
+++ shared/history.py
@@ -5,18 +5,26 @@
 #
 import tcc, gc, chains
 from utils import B2A
-#from ustruct import unpack_from, unpack, pack
+from ustruct import pack, unpack
 #from ubinascii import hexlify as b2a_hex
 from exceptions import IncorrectUTXOAmount
-from ubinascii import b2a_base64
+from ubinascii import b2a_base64, a2b_base64
 from serializations import COutPoint, uint256_from_str
 from main import settings
 
-# Very limited space in flash, so we limit how much to store there, and compress
-# as much as possible...
-HISTORY_DEPTH = const(30)
+# Very limited space in flash, so we compress as much as possible:
+# - would be bad for privacy to store these **UTXO amounts** in plaintext
+# - result is stored in a JSON serialization, so needs to be text encoded
+# - using base64, in two parts, concatenated
+#       - 18 bytes are hash over txnhash:out_num => base64 => 24 chars text
+#       - 8 bytes exact satoshi value => base64 (pad trimmed) => 11 chars
+# - stored satoshi value is XOR'ed with LSB from prevout txn hash, which isn't stored
+# - result is a 35 character string for each history entry
+# - if we store 24 of those it's about 23% of total setting space
+#
+HISTORY_DEPTH = const(24)
 
-# length of hashed&encoded key
+# length of hashed&encoded key only (base64(18 bytes) => 24)
 ENCKEY_LEN = 24
 
 class OutptValueCache:
@@ -41,6 +49,23 @@ class OutptValueCache:
         return b2a_base64(md.digest()[:18])[:-1].decode()
 
     @classmethod
+    def encode_value(cls, prevout, amt):
+        # XOR stored value with 64 LSB of original txnhash
+        xor = pack('<Q', prevout.hash & ((1<<64)-1))
+        val = bytes(i^j for i,j in zip(xor, pack('<Q', amt)))
+        assert len(val) == 8
+        return b2a_base64(val)[:-2].decode()
+
+    @classmethod
+    def decode_value(cls, prevout, text):
+        # base64 decode, xor w/ hash, decode as uint64
+        xor = pack('<Q', prevout.hash & ((1<<64)-1))
+        val = a2b_base64(text + '=')
+        assert len(val) == 8
+        val = bytes(i^j for i,j in zip(xor, val))
+        return unpack('<Q', val)[0]
+
+    @classmethod
     def fetch_amount(cls, prevout):
         # read the amount we expect, if we have it, else None
         vals = settings.get(cls.KEY)
@@ -50,7 +75,8 @@ class OutptValueCache:
         key = cls.encode_key(prevout)
         for v in vals:
             if v[0:ENCKEY_LEN] == key:
-                return int(v[ENCKEY_LEN:])
+                return cls.decode_value(prevout, v[ENCKEY_LEN:])
+
         return None
 
     @classmethod
@@ -84,7 +110,7 @@ class OutptValueCache:
 
         assert len(key) == ENCKEY_LEN
         assert amount > 0
-        vals.append(key + str(amount))
+        vals.append(key + cls.encode_value(prevout, amount))
 
         settings.set(cls.KEY, vals)
 
