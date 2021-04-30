8ece5b0320bb5901939637c66a388d3d72b39561
Peter D. Gray <peter@conalgo.com>
2021-04-30 10:44:46 -0400

    For 2021-04-30T1444-v4.1.1

--- stm32/COLDCARD/file_time.c
+++ stm32/COLDCARD/file_time.c
@@ -3,12 +3,12 @@
 //
 // AUTO-generated.
 //
-//   built: 2021-04-29
-// version: 4.1.0
+//   built: 2021-04-30
+// version: 4.1.1
 //
 #include <stdint.h>
 
 // this overrides ports/stm32/fatfs_port.c
 uint32_t get_fattime(void) {
-    return 0x529d2020UL;
+    return 0x529e2020UL;
 }
