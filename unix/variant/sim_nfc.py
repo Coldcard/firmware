1469e942b488280db1df7dcb9f9d152d88c9b1b8
Peter D. Gray <peter@conalgo.com>
2021-09-07 08:16:24 -0400

    simulate nfc hook

---
+++ unix/variant/sim_nfc.py
@@ -0,0 +1,39 @@
+# Replace NFC tag chip w/ emulation
+from nfc import NFCHandler
+
+global TAG_DATA
+TAG_DATA = bytearray(8196)
+
+class SimulatedNFCHandler(NFCHandler):
+    def __init__(self):
+        self.rf_on = False
+        self.i2c = NotImplementedError
+        self.mem_size = len(TAG_DATA)
+
+    # flash memory access (fixed tag data): 0x0 to 0x2000
+    def read(self, offset, count):
+        return bytes(TAG_DATA[offset:offset+count])
+
+    def write(self, offset, data):
+        TAG_DATA[offset:offset+len(data)] = data
+
+    def big_write(self, data):
+        self.write(0, data)
+
+    def is_rf_disabled(self):
+        # not checking if disable/sleep vs. off
+        return not self.rf_on
+
+    def set_rf_disable(self, val):
+        # using stronger "off" rather than sleep/disable
+        self.rf_on = not val
+        
+    def firsttime_setup(self):
+        print("simNFC: first time (skip)")
+        return
+
+    def setup(self):
+        # check if present, alive
+        print("simNFC: setup")
+
+# EOF
