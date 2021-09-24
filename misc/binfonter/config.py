0965c1c8e8331954291d0836eec59e04bda81ba0
Peter D. Gray <peter@conalgo.com>
2021-09-24 08:35:18 -0400

    Reference

---
+++ misc/binfonter/config.py
@@ -0,0 +1,64 @@
+font_files = {
+    'small': 'assets/zevv-peep-iso8859-15-07x14.bdf',
+    'large': 'assets/zevv-peep-iso8859-15-10x20.bdf',
+    'tiny': 'assets/4x6.bdf',
+}
+
+# test with:
+#   ./build.py build --portable && ./testit.py --msg "hello→world←\n↳this\n•Bullet\n•Text" -f small
+#
+special_chars = dict(small=[
+('→', dict(y=0), '''\
+
+
+
+          
+          x
+          xx
+  xxxxxxxxxxx 
+          xx
+          x
+'''),
+('←', dict(y=0), '''\
+
+
+
+          
+    x      
+   xx       
+  xxxxxxxxxxx 
+   xx       
+    x      
+'''),
+('↳', dict(y=0), '''\
+
+   x 
+   x 
+   x      
+   x      x
+   x      xx
+   xxxxxxxxxx 
+          xx
+          x
+'''),
+('•', dict(y=0), '''\
+
+
+
+
+
+      xxx  
+      xxx 
+      xxx 
+'''),
+('-', dict(y=0), '''\
+
+
+
+
+
+
+  xxxxx 
+'''),
+
+])
