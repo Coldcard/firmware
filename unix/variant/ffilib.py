019889321296d1abb72d377e78dfbe0c00e39a5f
avirgovi <avirgovi@cisco.com>
2022-03-18 14:28:25 +0100

    more proper os.platform differentiation

--- unix/variant/ffilib.py
+++ unix/variant/ffilib.py
@@ -14,7 +14,8 @@ def open(name, maxver=10, extra=()):
     except KeyError:
         pass
     def libs():
-        if sys.platform == "linux":
+        # TODO what about sys.platform == "darwin" ??
+        if sys.platform in ["linux", "linux2", "coldcard-unix"]:
             yield '%s.so' % name
             for i in range(maxver, -1, -1):
                 yield '%s.so.%u' % (name, i)
