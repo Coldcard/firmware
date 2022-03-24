887bc410cefad040c955e7257279ab4491ba5be8
Peter D. Gray <peter@conalgo.com>
2022-03-24 12:46:05 -0400

    For 2022-03-24T1645-v5.0.1

--- stm32/COLDCARD/file_time.c
+++ stm32/COLDCARD/file_time.c
@@ -1,14 +1,13 @@
-
-// (c) Copyright 2020 by Coinkite Inc. This file is covered by license found in COPYING-CC.
+// (c) Copyright 2020-2022 by Coinkite Inc. This file is covered by license found in COPYING-CC.
 //
 // AUTO-generated.
 //
-//   built: 2022-01-13
-// version: 5.0.0
+//   built: 2022-03-24
+// version: 5.0.1
 //
 #include <stdint.h>
 
 // this overrides ports/stm32/fatfs_port.c
 uint32_t get_fattime(void) {
-    return 0x542d2800UL;
+    return 0x54782800UL;
 }
