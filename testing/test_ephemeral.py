0c80cb354781a3f2d42ceb190c15c230af6ebc17
Peter D. Gray <peter@conalgo.com>
2022-10-04 10:49:24 -0400

    updates, add dice test

--- testing/test_ephemeral.py
+++ testing/test_ephemeral.py
@@ -1,3 +1,7 @@
+# (c) Copyright 2022 by Coinkite Inc. This file is covered by license found in COPYING-CC.
+#
+# Ephemeral Seeds tests
+#
 import pytest, time, re
 
 from constants import simulator_fixed_words, simulator_fixed_xfp, simulator_fixed_xpub
@@ -46,49 +50,70 @@ def get_identity_story(goto_home, pick_menu_item, cap_story):
         return story
     return doit
 
+@pytest.fixture
+def goto_eph_seed_menu(goto_home, pick_menu_item, cap_story, need_keypress):
+    def doit():
+        goto_home()
+        pick_menu_item("Advanced/Tools")
+        pick_menu_item("Ephemeral Seed")
+
+        title, story = cap_story()
+        if title == "WARNING":
+            assert "temporary secret stored solely in device RAM" in story
+            assert "Press 4 to prove you read to the end of this message and accept all consequences." in story
+            need_keypress("4")  # understand consequences
+    return doit
 
 @pytest.mark.parametrize("num_words", ["12", "24"])
+@pytest.mark.parametrize("dice", [False, True])
 def test_ephemeral_seed_generate(num_words, cap_menu, pick_menu_item, goto_home, cap_story, need_keypress,
-                                 reset_seed_words, get_seed_value_ux, get_identity_story, fake_txn, dev, try_sign):
+                                 reset_seed_words, get_seed_value_ux, get_identity_story, fake_txn, dev, try_sign, goto_eph_seed_menu, dice):
+
     reset_seed_words()
-    seed_words = get_seed_value_ux()
-    assert seed_words == simulator_fixed_words.split(" ")
-    goto_home()
-    pick_menu_item("Advanced/Tools")
-    pick_menu_item("Ephemeral Seed")
+    goto_eph_seed_menu()
+
     menu = cap_menu()
-    # no ephemeral seed chosen
+
+    # no ephemeral seed chosen (yet)
     assert len(menu) == 2
-    pick_menu_item("Generate")
-    pick_menu_item(f"{num_words} Words")
-    time.sleep(0.1)
+    pick_menu_item("Generate Seed")
+    if not dice:
+        pick_menu_item(f"{num_words} Words")
+        time.sleep(0.1)
+    else:
+        pick_menu_item(f"{num_words} Word Dice Roll")
+        for ch in '123456yy':
+            need_keypress(ch)
+
     title, story = cap_story()
     assert f"Record these {num_words} secret words!" in story
     assert "Press 6 to skip word quiz" in story
+
     # filter those that starts with space, number and colon --> actual words
     e_seed_words = seed_story_to_words(story)
     assert len(e_seed_words) == int(num_words)
+
     need_keypress("6")  # skip quiz
     need_keypress("y")  # yes - I'm sure
     time.sleep(0.1)
-    title, story = cap_story()
-    assert title == "WARNING"
-    assert "Ephemeral seed is a temporary secret stored solely in device RAM" in story
-    assert "Press 4 to prove you read to the end of this message and accept all consequences." in story
     need_keypress("4")  # understand consequences
     time.sleep(0.1)
     title, story = cap_story()
     in_effect_xfp = story[1:9]
-    assert "New master key in effect until next power down." in story
+    assert "key in effect until next power down." in story
     need_keypress("y")  # just confirm new master key message
+
     menu = cap_menu()
     assert menu[0] == "Ready To Sign"  # returned to main menu
     seed_words = get_seed_value_ux()
     assert e_seed_words == seed_words
+
     ident_story = get_identity_story()
     assert "Ephemeral seed is in effect" in ident_story
+
     ident_xfp = ident_story.split("\n\n")[1].strip()
     assert ident_xfp == in_effect_xfp
+
     e_master_xpub = dev.send_recv(CCProtocolPacker.get_xpub(), timeout=5000)
     assert e_master_xpub != simulator_fixed_xpub
     psbt = fake_txn(3, 3, master_xpub=e_master_xpub, segwit_in=True)
@@ -97,25 +122,15 @@ def test_ephemeral_seed_generate(num_words, cap_menu, pick_menu_item, goto_home,
     pick_menu_item("Advanced/Tools")
     pick_menu_item("Ephemeral Seed")
     menu = cap_menu()
-    # ephemeral seed chosen -> CLEAR [xfp] MUST be visible
+
+    # ephemeral seed chosen -> [xfp] will be visible
     assert len(menu) == 3
-    do_clear = menu[2]
-    assert do_clear == f"CLEAR [{ident_xfp}]"
-    pick_menu_item(do_clear)
-    need_keypress("y")  # confirm removal of ephemeral seed
-    seed_words = get_seed_value_ux()
-    assert seed_words == simulator_fixed_words.split(" ")
-    ident_story = get_identity_story()
-    ident_xfp = ident_story.split("\n\n")[1].strip()
-    assert xfp2str(simulator_fixed_xfp) == ident_xfp
-    assert "Ephemeral seed is in effect" not in ident_story
-    assert dev.send_recv(CCProtocolPacker.get_xpub(), timeout=5000) == simulator_fixed_xpub
-    with pytest.raises(Exception):
-        # MUST raise psbt contains ephemeral xpub but currently we are at fixed simulator seed
-        try_sign(psbt, accept=True, finalize=True)
-    goto_home()
-    pick_menu_item("Advanced/Tools")
-    pick_menu_item("Ephemeral Seed")
+    assert menu[0] == f"[{ident_xfp}]"
+
+    reset_seed_words()
+
+    goto_eph_seed_menu()
     menu = cap_menu()
     assert len(menu) == 2
-    goto_home()
+
+# EOF
