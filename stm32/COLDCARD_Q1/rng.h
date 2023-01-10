2f2a56bd1245daaa902b9bc49b96e2833b615240
Peter D. Gray <peter@conalgo.com>
2023-01-10 14:33:39 -0500

    pull basics from mk4

---
+++ stm32/COLDCARD_Q1/rng.h
@@ -0,0 +1,8 @@
+/*
+ * (c) Copyright 2018 by Coinkite Inc. This file is covered by license found in COPYING-CC.
+ */
+#pragma once
+
+MP_DECLARE_CONST_FUN_OBJ_0(pyb_rng_get_obj);
+MP_DECLARE_CONST_FUN_OBJ_1(pyb_rng_get_bytes_obj);
+
