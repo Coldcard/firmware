0493aee1aee52e35ad59d7829e5a27c9db13b6a4
Peter D. Gray <peter@conalgo.com>
2023-06-01 15:13:17 -0400

    add KEY_CLEAR

--- shared/charcodes.py
+++ shared/charcodes.py
@@ -21,7 +21,7 @@ KEY_QR = '\x11'         # ctrl-Q
 KEY_TAB = '\t'          # tab = ctrl-I
 KEY_SELECT = '\r'       # = CR
 KEY_CANCEL = '\x1b'     # ESC = Cancel
-KEY_LEFT = '\x15'           # ^U = left (incompatible)
+KEY_LEFT = '\x03'           # ^B = left (incompatible)
 KEY_UP = '\x0b'             # ^K = up on ADM-3A
 KEY_RIGHT = '\x0c'          # ^L = right on ADM-3A
 KEY_DOWN = '\x0a'           # ^J = LF = down on ADM-3A
@@ -36,7 +36,8 @@ KEY_LAMP = '\x07'           # BELL = ^G
 KEY_SHIFT = '\x01'
 KEY_SPACE = ' '
 KEY_SYMBOL = '\x02'
-KEY_DELETE = '\x08'           # ^H = backspace
+KEY_DELETE = '\x08'          # ^H = backspace
+KEY_CLEAR = '\x15'           # ^U = clear entry
 
 # function keys, filling gaps, running out of space!
 KEY_F1 = '\x0f'
@@ -61,15 +62,16 @@ DECODER = (KEY_NFC + KEY_QR + KEY_TAB
 # - same when shift is down
 # - make some unmarked combos dead (like shift+UP)
 # - but shift+' can be ", which really should be symb+'
+# - undocumented: shift+DEL => CLEAR
 DECODER_SHIFT = (
     '\0\0\0\0\0\0\0\0\0\0'
     + '!@#$%^&*()'
     + 'QWERTYUIOP'
     + 'ASDFGHJKL"'
     + 'ZXCVBNM<>?'
-    '\0\0\0\0\0\0\0\0\0\0' )
+    '\0\0\0\0' + KEY_CLEAR + '\0\0\0\0\0' )
 
-# - in caps mode: numbers unaffected, and also allow meta keys normally
+# in caps mode: numbers unaffected, and also allow meta keys normally
 DECODER_CAPS = (KEY_NFC + KEY_QR + KEY_TAB 
             + KEY_LEFT + KEY_UP + KEY_DOWN + KEY_RIGHT + KEY_SELECT + KEY_CANCEL + '\0'
     + '1234567890'
@@ -78,7 +80,7 @@ DECODER_CAPS = (KEY_NFC + KEY_QR + KEY_TAB
     + 'ZXCVBNM,./'
     + KEY_LAMP + KEY_SHIFT + KEY_SPACE + KEY_SYMBOL + KEY_DELETE + '\0\0\0\0\0')
 
-# - same w/ SYMBOL pressed
+# when SYMBOL pressed
 # - be nice and allow number+symbol == number + shift
 DECODER_SYMBOL = (KEY_NFC + KEY_QR + KEY_TAB 
             + KEY_HOME + KEY_PAGE_UP + KEY_PAGE_DOWN + KEY_END + KEY_SELECT + KEY_CANCEL + '\0'
