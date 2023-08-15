74a6d00c42a6a77a7f22648de2fb86a30e15591b
Peter D. Gray <peter@conalgo.com>
2023-08-15 13:05:16 -0400

    save mem on mk4

--- shared/charcodes.py
+++ shared/charcodes.py
@@ -8,12 +8,11 @@
 #
 try:
     from micropython import const
+    from version import has_qwerty
 except ImportError:
     # this file also used by simulator.py and conftest.py
     const = int
-
-NUM_ROWS = const(6)
-NUM_COLS = const(10)
+    has_qwerty = True
 
 # ascii key codes;
 KEY_NFC = '\x0e'        # ctrl-N
@@ -47,58 +46,64 @@ KEY_F4 = '\x14'
 KEY_F5 = '\x16'
 KEY_F6 = '\x17'
 
-# (row, col) => keycode
-# - unused spots are \0
-# - these are unshifted values
-# - ten per row, gaps with zero
-DECODER = (KEY_NFC + KEY_TAB + KEY_QR 
-            + KEY_LEFT + KEY_UP + KEY_DOWN + KEY_RIGHT + KEY_CANCEL + KEY_SELECT + '\0'
-    + '1234567890'
-    + 'qwertyuiop'
-    + 'asdfghjkl`'
-    + 'zxcvbnm,./'
-    + KEY_LAMP + KEY_SHIFT + KEY_SPACE + KEY_SYMBOL + KEY_DELETE + '\0\0\0\0\0')
+# save memory on Mk4
+if has_qwerty:
+    NUM_ROWS = const(6)
+    NUM_COLS = const(10)
 
-# - same when shift is down
-# - make some unmarked combos dead (like shift+UP)
-# - but shift+' can be ", which really should be symb+'
-# - undocumented: shift+DEL => CLEAR
-DECODER_SHIFT = (
-    '\0\0\0\0\0\0\0\0\0\0'
-    + '!@#$%^&*()'
-    + 'QWERTYUIOP'
-    + 'ASDFGHJKL"'
-    + 'ZXCVBNM<>?'
-    '\0\0\0\0' + KEY_CLEAR + '\0\0\0\0\0' )
+    # (row, col) => keycode
+    # - unused spots are \0
+    # - these are unshifted values
+    # - ten per row, gaps with zero
+    DECODER = (KEY_NFC + KEY_TAB + KEY_QR 
+                + KEY_LEFT + KEY_UP + KEY_DOWN + KEY_RIGHT + KEY_CANCEL + KEY_SELECT + '\0'
+        + '1234567890'
+        + 'qwertyuiop'
+        + 'asdfghjkl`'
+        + 'zxcvbnm,./'
+        + KEY_LAMP + KEY_SHIFT + KEY_SPACE + KEY_SYMBOL + KEY_DELETE + '\0\0\0\0\0')
 
-# in caps mode: numbers unaffected, and also allow meta keys normally
-DECODER_CAPS = (KEY_NFC + KEY_TAB + KEY_QR
-            + KEY_LEFT + KEY_UP + KEY_DOWN + KEY_RIGHT + KEY_CANCEL + KEY_SELECT + '\0'
-    + '1234567890'
-    + 'QWERTYUIOP'
-    + "ASDFGHJKL'"
-    + 'ZXCVBNM,./'
-    + KEY_LAMP + KEY_SHIFT + KEY_SPACE + KEY_SYMBOL + KEY_DELETE + '\0\0\0\0\0')
+    # - same when shift is down
+    # - make some unmarked combos dead (like shift+UP)
+    # - but shift+' can be ", which really should be symb+'
+    # - undocumented: shift+DEL => CLEAR
+    DECODER_SHIFT = (
+        '\0\0\0\0\0\0\0\0\0\0'
+        + '!@#$%^&*()'
+        + 'QWERTYUIOP'
+        + 'ASDFGHJKL"'
+        + 'ZXCVBNM<>?'
+        '\0\0\0\0' + KEY_CLEAR + '\0\0\0\0\0' )
 
-# when SYMBOL pressed
-# - be nice and allow number+symbol == number + shift
-# - also nicity: symb+cancel=clear
-DECODER_SYMBOL = (KEY_NFC + KEY_TAB + KEY_QR 
-            + KEY_HOME + KEY_PAGE_UP + KEY_PAGE_DOWN + KEY_END + KEY_CANCEL + KEY_SELECT + '\0'
-    + '!@#$%^&*()'
-    + '-_`\0\0\0[]{}'
-    + '+=\0\0:;~|\\"'
-    + KEY_F1 + KEY_F2 + KEY_F3 + KEY_F4 + KEY_F5 + KEY_F6 + '\0<>?'
-    '\0\0\0\0' + KEY_CLEAR + '\0\0\0\0\0' )
+    # in caps mode: numbers unaffected, and also allow meta keys normally
+    DECODER_CAPS = (KEY_NFC + KEY_TAB + KEY_QR
+                + KEY_LEFT + KEY_UP + KEY_DOWN + KEY_RIGHT + KEY_CANCEL + KEY_SELECT + '\0'
+        + '1234567890'
+        + 'QWERTYUIOP'
+        + "ASDFGHJKL'"
+        + 'ZXCVBNM,./'
+        + KEY_LAMP + KEY_SHIFT + KEY_SPACE + KEY_SYMBOL + KEY_DELETE + '\0\0\0\0\0')
 
-KEYNUM_LAMP = const(50)
-KEYNUM_SHIFT = const(51)
-KEYNUM_SYMBOL = const(53)
+    # when SYMBOL pressed
+    # - be nice and allow number+symbol == number + shift
+    # - also nicity: symb+cancel=clear
+    DECODER_SYMBOL = (KEY_NFC + KEY_TAB + KEY_QR 
+                + KEY_HOME + KEY_PAGE_UP + KEY_PAGE_DOWN + KEY_END + KEY_CANCEL + KEY_SELECT + '\0'
+        + '!@#$%^&*()'
+        + '-_`\0\0\0[]{}'
+        + '+=\0\0:;~|\\"'
+        + KEY_F1 + KEY_F2 + KEY_F3 + KEY_F4 + KEY_F5 + KEY_F6 + '\0<>?'
+        '\0\0\0\0' + KEY_CLEAR + '\0\0\0\0\0' )
 
-assert len(DECODER) == NUM_ROWS * NUM_COLS
-assert len(DECODER_SYMBOL) == NUM_ROWS * NUM_COLS
-assert len(DECODER_SHIFT) == NUM_ROWS * NUM_COLS
-assert DECODER[KEYNUM_SHIFT] == KEY_SHIFT
-assert DECODER[KEYNUM_SYMBOL] == KEY_SYMBOL
-assert DECODER[KEYNUM_LAMP] == KEY_LAMP
+    KEYNUM_LAMP = const(50)
+    KEYNUM_SHIFT = const(51)
+    KEYNUM_SYMBOL = const(53)
 
+    assert len(DECODER) == NUM_ROWS * NUM_COLS
+    assert len(DECODER_SYMBOL) == NUM_ROWS * NUM_COLS
+    assert len(DECODER_SHIFT) == NUM_ROWS * NUM_COLS
+    assert DECODER[KEYNUM_SHIFT] == KEY_SHIFT
+    assert DECODER[KEYNUM_SYMBOL] == KEY_SYMBOL
+    assert DECODER[KEYNUM_LAMP] == KEY_LAMP
+
+# EOF
