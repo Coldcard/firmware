5393e924ba8bed3a1a8f69f8ed59d754b757525a
Peter D. Gray <peter@conalgo.com>
2023-09-08 16:06:38 -0400

    Bugfix: firmware sizes must align with flash erase unit (4k)

--- stm32/COLDCARD/file_time.c
+++ stm32/COLDCARD/file_time.c
@@ -2,12 +2,12 @@
 //
 // AUTO-generated.
 //
-//   built: 2023-02-03
-// version: 5.1.0
+//   built: 2023-09-08
+// version: 5.1.4
 //
 #include <stdint.h>
 
 // this overrides ports/stm32/fatfs_port.c
 uint32_t get_fattime(void) {
-    return 0x56432820UL;
+    return 0x57282820UL;
 }
--- stm32/COLDCARD_MK4/file_time.c
+++ stm32/COLDCARD_MK4/file_time.c
@@ -2,12 +2,12 @@
 //
 // AUTO-generated.
 //
-//   built: 2023-09-07
-// version: 5.1.3
+//   built: 2023-09-08
+// version: 5.1.4
 //
 #include <stdint.h>
 
 // this overrides ports/stm32/fatfs_port.c
 uint32_t get_fattime(void) {
-    return 0x57272820UL;
+    return 0x57282820UL;
 }
