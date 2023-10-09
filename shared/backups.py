241bbb545faea9cd2c953029216d0b8fe1061088
scgbckbone <scgbckbone@proton.me>
2023-10-09 02:35:28 +0200

    seed vault backup tests
    (cherry picked from commit bb6fea731ef8b9b5439d3e1b281ad082d3ba4632)

--- shared/backups.py
+++ shared/backups.py
@@ -100,6 +100,8 @@ def render_backup_contents(bypass_tmp=False):
         if k == 'bkpw': continue        # confusing/circular
         if k == 'sd2fa': continue       # do NOT backup SD 2FA (card can be lost or damaged)
         if k == 'words': continue       # words length is recalculated from secret
+        if k == 'seedvault' and not v: continue
+        if k == 'seeds' and not v: continue
         ADD('setting.' + k, v)
 
     import hsm
