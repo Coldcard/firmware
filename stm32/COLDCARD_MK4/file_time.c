4cbc9329675afb7ae6aa874af23d69c491b1de8a
Peter D. Gray <peter@conalgo.com>
2023-12-18 11:36:44 -0500

    New release: 2023-12-18T1636-v5.2.1
    (cherry picked from commit 5cb63b299d8d254398b1831c03827b11245ee6f5)

--- stm32/COLDCARD_MK4/file_time.c
+++ stm32/COLDCARD_MK4/file_time.c
@@ -2,12 +2,12 @@
 //
 // AUTO-generated.
 //
-//   built: 2023-10-10
-// version: 5.2.0
+//   built: 2023-12-18
+// version: 5.2.1
 //
 #include <stdint.h>
 
 // this overrides ports/stm32/fatfs_port.c
 uint32_t get_fattime(void) {
-    return 0x574a2840UL;
+    return 0x57922840UL;
 }
