b8f69d39cec79675e3da2cc0a1670141c4b4a1fd
Peter D. Gray <peter@conalgo.com>
2024-02-27 11:35:25 -0500

    date

--- testing/login_settings_tests.py
+++ testing/login_settings_tests.py
@@ -1,4 +1,4 @@
-# (c) Copyright 2020 by Coinkite Inc. This file is covered by license found in COPYING-CC.
+# (c) Copyright 2024 by Coinkite Inc. This file is covered by license found in COPYING-CC.
 #
 # to run it on both Mk4 and Q:
 #   python login_settings_tests.py; sleep 10; python --Q login_settings_tests.py
