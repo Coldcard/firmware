ef97f9e1316348386060b0031295cd5e903aa9b0
Peter D. Gray <peter@conalgo.com>
2024-04-17 09:35:08 -0400

    Add notice

--- unix/variant/sim_scanner.py
+++ unix/variant/sim_scanner.py
@@ -1,5 +1,8 @@
+# (c) Copyright 2023 by Coinkite Inc. This file is covered by license found in COPYING-CC.
+#
 # Replace QR Scanner module interface.
-# TODO: support (optional) local real scanner over USB serial.
+#
+# - also supports local real scanner over USB serial.
 #
 import os, sys
 import uasyncio as asyncio
