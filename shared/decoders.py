dd697b7da3505254ff95258a9be39b41ec48feb1
scgbckbone <scgbckbone@proton.me>
2024-05-29 15:13:53 +0200

    compile regex for multi use

--- shared/decoders.py
+++ shared/decoders.py
@@ -195,11 +195,12 @@ def decode_short_text(got):
     # [0-9a-fA-F]{8}\s*:\s*[xtyYzZuUvV]pub[1-9A-HJ-NP-Za-km-z]{107}
     # above is more precise BUT counted repetitions not supported in mpy
     cc_ms_pat = r"[0-9a-fA-F]+\s*:\s*[xtyYzZuUvV]pub[1-9A-HJ-NP-Za-km-z]+"
+    rgx = ure.compile(cc_ms_pat)
     # go line by line and match above, once 2 matches observed - considered multisig
     # important to not use ure.search for big strings (can run out of stack)
     c = 0  # match count
     for l in got.split("\n"):
-        if ure.search(cc_ms_pat, l):
+        if rgx.search(l):
             c += 1
         if c > 1:
             return 'multi', (got,)
