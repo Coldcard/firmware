6445fad042813ece19df3a7ef60bf0135e3e5015
Peter D. Gray <peter@conalgo.com>
2024-10-15 09:36:30 -0400

    lowercase

--- shared/ccc.py
+++ shared/ccc.py
@@ -78,8 +78,7 @@ class CCCFeature:
         # a very basic an permissive policy, but non-zero too.
         # - 1BTC per day
         chain = chains.current_chain()
-        return dict(mag=1, vel=144, block_h=chain.CCC_MIN_BLOCK,
-                    web2fa='', addrs=[])
+        return dict(mag=1, vel=144, block_h=chain.ccc_min_block, web2fa='', addrs=[])
 
     @classmethod
     def get_policy(cls):
@@ -141,7 +140,7 @@ class CCCFeature:
                 # this is unix timestamp - not allowed - fail
                 raise CCCPolicyViolationError("nLockTime not block height")
 
-            block_h = pol.get("block_h", chains.current_chain().CCC_MIN_BLOCK)
+            block_h = pol.get("block_h", chains.current_chain().ccc_min_block)
             if psbt.lock_time <= block_h:
                 raise CCCPolicyViolationError("rewound")
 
