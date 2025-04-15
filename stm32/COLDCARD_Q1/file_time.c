6a3c8ae67633a11fdd88c03095126d2a2a358e67
Peter D. Gray <peter@conalgo.com>
2025-04-15 10:06:16 -0400

    New bootrom version releases

--- stm32/COLDCARD_Q1/file_time.c
+++ stm32/COLDCARD_Q1/file_time.c
@@ -2,12 +2,12 @@
 //
 // AUTO-generated.
 //
-//   built: 2025-02-26
-// version: 1.4.0Q
+//   built: 2025-04-15
+// version: 1.3.2Q
 //
 #include <stdint.h>
 
 // this overrides ports/stm32/fatfs_port.c
 uint32_t get_fattime(void) {
-    return 0x5a5a0880UL;
+    return 0x5a8f0860UL;
 }
