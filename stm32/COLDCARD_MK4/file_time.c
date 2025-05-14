6a5311e8f5444a4a4c892aad86c4b4ae6cbfab2a
Peter D. Gray <peter@conalgo.com>
2025-05-14 09:35:59 -0400

    new release

--- stm32/COLDCARD_MK4/file_time.c
+++ stm32/COLDCARD_MK4/file_time.c
@@ -2,12 +2,12 @@
 //
 // AUTO-generated.
 //
-//   built: 2025-04-15
-// version: 5.4.2
+//   built: 2025-05-13
+// version: 5.4.3
 //
 #include <stdint.h>
 
 // this overrides ports/stm32/fatfs_port.c
 uint32_t get_fattime(void) {
-    return 0x5a8f2880UL;
+    return 0x5aad2880UL;
 }
--- stm32/COLDCARD_Q1/file_time.c
+++ stm32/COLDCARD_Q1/file_time.c
@@ -2,12 +2,12 @@
 //
 // AUTO-generated.
 //
-//   built: 2025-04-15
-// version: 1.3.2Q
+//   built: 2025-05-13
+// version: 1.3.3Q
 //
 #include <stdint.h>
 
 // this overrides ports/stm32/fatfs_port.c
 uint32_t get_fattime(void) {
-    return 0x5a8f0860UL;
+    return 0x5aad0860UL;
 }
