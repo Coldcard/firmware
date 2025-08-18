d3caf6326506d80be85c352012c17807543d919c
Peter D. Gray <peter@conalgo.com>
2025-08-18 08:57:27 -0400

    planning

--- testing/test_hobble.py
+++ testing/test_hobble.py
@@ -18,10 +18,30 @@ from test_backup import make_big_notes
     - cannot export
     - cannot edit
     - can view / use for kbd emulation
+- check notes not offered if none defined
+- check readonly features on notes when note pre-defined before entering hobbled mode
+- notes hidden if the exist but access disabled in policy
+
 - check KT only offered if MS wallet setup
 - scan a KT and have it rejected if not PSBT type: so R and E types
 - MS psbt KT should still work in hobbled mode: test_teleport.py::test_teleport_ms_sign
-- verify no settings menu
 
+- verify no settings menu
+- temp seeds are read only: no create, no rename, etc.
+- seed vault can be accessed tho
+
+- login sequence
+    1) system has lgto value: should get bypass pin, main pin, delay, then main pin again
+    2) using a trick PIN with delay, after bypass pin should delay
+    3) bypass pin + duress wallet PIN => should work => but not a useful trick combo
+   
+- word entry during login
+    - q1 vs mk4 style
+    - wrong values given, etc
+
+- update menu tree w/ hobble mode view
+
+- verify whitelist of QR types is correct when in hobbled mode
+    - no private key material, no teleport starting, unless "okeys" is set
 
 '''
