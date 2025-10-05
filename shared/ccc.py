60ccbf2cb1bdeaa8aa4417a582c3c723c82c6e93
scgbckbone <scgbckbone@proton.me>
2025-10-05 04:11:12 +0200

    remove unused import from SSSP menu constructor

--- shared/ccc.py
+++ shared/ccc.py
@@ -1189,8 +1189,6 @@ class SSSPConfigMenu(MenuSystem):
         self.replace_items(tmp)
 
     def construct(self):
-        from multisig import MultisigWallet, make_ms_wallet_menu
-
         items = [
             #         xxxxxxxxxxxxxxxx
             MenuItem('Edit Policy...', 
