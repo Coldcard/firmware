147e191456a2f77553824a95adf05bca5165d3c6
Peter D. Gray <peter@conalgo.com>
2025-11-25 11:17:19 -0500

    For 2025-11-25T1617-v6.4.1QX

--- stm32/COLDCARD_Q1/file_time.c
+++ stm32/COLDCARD_Q1/file_time.c
@@ -2,12 +2,12 @@
 //
 // AUTO-generated.
 //
-//   built: 2025-11-05
-// version: 6.4.0QX
+//   built: 2025-11-25
+// version: 6.4.1QX
 //
 #include <stdint.h>
 
 // this overrides ports/stm32/fatfs_port.c
 uint32_t get_fattime(void) {
-    return 0x5b653080UL;
+    return 0x5b793080UL;
 }
