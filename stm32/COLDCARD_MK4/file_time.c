ed5b7b1b63f3c1bfe695ca8805809fe6045019c3
Peter D. Gray <peter@conalgo.com>
2026-03-25 10:05:50 -0400

    bump

--- stm32/COLDCARD_MK4/file_time.c
+++ stm32/COLDCARD_MK4/file_time.c
@@ -1,13 +1,13 @@
-// (c) Copyright 2020-2025 by Coinkite Inc. This file is covered by license found in COPYING-CC.
+// (c) Copyright 2020-2026 by Coinkite Inc. This file is covered by license found in COPYING-CC.
 //
 // AUTO-generated.
 //
-//   built: 2025-11-25
-// version: 6.4.1X
+//   built: 2026-03-23
+// version: 6.5.0X
 //
 #include <stdint.h>
 
 // this overrides ports/stm32/fatfs_port.c
 uint32_t get_fattime(void) {
-    return 0x5b793080UL;
+    return 0x5c7730a0UL;
 }
