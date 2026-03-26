fdf630ab315c02cc82a3bb8a87aa71f6e8bbda75
scgbckbone <scgbckbone@proton.me>
2026-03-26 11:46:40 +0100

    Wait UX for chain changes

--- shared/actions.py
+++ shared/actions.py
@@ -2366,6 +2366,8 @@ async def change_seed_vault(is_enabled):
 
 async def change_which_chain(*a):
     # setting already changed, but reflect that value in other settings
+    from glob import dis
+    dis.fullscreen("Wait...")
     try:
         # update xpub stored in settings
         import stash
