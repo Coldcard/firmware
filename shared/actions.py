Coldcard/firmware

commit 300323f18d20d6e0a904f5f3bcc9d63d84d2f6a7

final solution can_cancel=True

shared/actions.py
@@ -1100,13 +1100,13 @@ async def export_xpub(label, _2, item):
         if ch == 'x': return
         if ch == "2":
             slip132 = not slip132
-            continue
+
         if ch == '1':
-            acct = await ux_enter_bip32_index('Account Number:') or 0
+            acct = await ux_enter_bip32_index('Account Number:')
+            if acct is None: continue
             pth_split = path.split("/")
             pth_split[-1] = ("%dh" % acct)
             path = "/".join(pth_split)
-            continue
 
         # assume zero account if not picked
         path = path.format(acct=acct)
@@ -1144,15 +1144,16 @@ async def electrum_skeleton(a, b, item):
 
     ch = await ux_show_story(electrum_export_story(title), escape='1')
 
-    account_num = 0
+    acct = 0
     if ch == '1':
-        account_num = await ux_enter_bip32_index('Account Number:') or 0
-    elif ch != 'y':
+        acct = await ux_enter_bip32_index('Account Number:')
+
+    if (ch != 'y') or acct is None:
         return
 
     rv = [
         MenuItem(chains.addr_fmt_label(af), f=electrum_skeleton_step2,
-                 arg=(af, account_num, title, fname_pat))
+                 arg=(af, acct, title, fname_pat))
         for af in chains.SINGLESIG_AF
     ]
     the_ux.push(MenuSystem(rv))
@@ -1173,13 +1174,14 @@ async def ss_descriptor_skeleton(_0, _1, item):
         int_ext, allowed_af, ll, f_pattern, direct_way = item.arg
         addition = " for " + ll
 
-    account_num = 0
+    acct = 0
     if not direct_way:
         ch = await ux_show_story(ss_descriptor_export_story(addition), escape='1')
 
         if ch == '1':
-            account_num = await ux_enter_bip32_index('Account Number:', unlimited=True) or 0
-        elif ch != 'y':
+            acct = await ux_enter_bip32_index('Account Number:', unlimited=True)
+
+        if (ch != 'y') or acct is None:
             return
 
     if int_ext is None:
@@ -1191,12 +1193,12 @@ async def ss_descriptor_skeleton(_0, _1, item):
         int_ext = False if ch == "1" else True
 
     if len(allowed_af) == 1:
-        await make_descriptor_wallet_export(allowed_af[0], account_num, int_ext=int_ext,
+        await make_descriptor_wallet_export(allowed_af[0], acct, int_ext=int_ext,
                                             fname_pattern=f_pattern, direct_way=direct_way)
     else:
         rv = [
             MenuItem(chains.addr_fmt_label(af), f=descriptor_skeleton_step2,
-                     arg=(af, account_num, int_ext, f_pattern, direct_way))
+                     arg=(af, acct, int_ext, f_pattern, direct_way))
             for af in allowed_af
         ]
         the_ux.push(MenuSystem(rv))
@@ -1210,12 +1212,13 @@ async def key_expression_skeleton_step2(_1, _2, item):
 async def key_expression_skeleton(_0, _1, item):
     # Export key expression -> [xfp/d/e/r]xpub
 
-    acct_num = 0
+    acct = 0
     ch = await ux_show_story("This saves a extended key expression."
                              + PICK_ACCOUNT + SENSITIVE_NOT_SECRET, escape='1')
     if ch == '1':
-        acct_num = await ux_enter_bip32_index('Account Number:', unlimited=True) or 0
-    elif ch != 'y':
+        acct = await ux_enter_bip32_index('Account Number:', unlimited=True)
+
+    if (ch != 'y') or acct is None:
         return
 
     # element on 2nd index is address format for signed exports
@@ -1235,7 +1238,7 @@ async def key_expression_skeleton(_0, _1, item):
 
     ct = chains.current_chain().b44_cointype
 
-    rv = [ MenuItem(label, f=key_expression_skeleton_step2, arg=(orig_der % (ct, acct_num), af))
+    rv = [ MenuItem(label, f=key_expression_skeleton_step2, arg=(orig_der % (ct, acct), af))
                     for label, orig_der, af in todo ]
     rv += [ MenuItem("Custom Path", menu=doit) ]
 
@@ -1285,14 +1288,15 @@ You can then run the commands in Bitcoin Core's console window, \
 without ever connecting this Coldcard to a computer.\
 ''' + PICK_ACCOUNT + SENSITIVE_NOT_SECRET, escape='1')
 
-    account_num = 0
+    acct = 0
     if ch == '1':
-        account_num = await ux_enter_bip32_index('Account Number:') or 0
-    elif ch != 'y':
+        acct = await ux_enter_bip32_index('Account Number:')
+
+    if (ch != 'y') or acct is None:
         return
 
     # no choices to be made, just do it.
-    await make_bitcoin_core_wallet(account_num)
+    await make_bitcoin_core_wallet(acct)
 
 
 async def electrum_skeleton_step2(_1, _2, item):
@@ -1306,13 +1310,14 @@ async def _generic_export(prompt, label, f_pattern):
     # like the Multisig export, make a single JSON file with
     # basically all useful XPUB's in it.
     ch = await ux_show_story(prompt + PICK_ACCOUNT + SENSITIVE_NOT_SECRET, escape="1")
-    account_num = 0
+    acct = 0
     if ch == '1':
-        account_num = await ux_enter_bip32_index('Account Number:') or 0
-    elif ch != 'y':
+        acct = await ux_enter_bip32_index('Account Number:')
+
+    if (ch != 'y') or acct is None:
         return
 
-    await export_contents(label, lambda: generate_generic_export(account_num),
+    await export_contents(label, lambda: generate_generic_export(acct),
                           f_pattern, is_json=True)
 
 async def generic_skeleton(*A):
@@ -1357,16 +1362,17 @@ async def unchained_capital_export(*a):
     ch = await ux_show_story('''\
 This saves multisig XPUB information required to setup on the Unchained platform. \
 ''' + PICK_ACCOUNT + SENSITIVE_NOT_SECRET, escape="1")
-    account_num = 0
+    acct = 0
     if ch == '1':
-        account_num = await ux_enter_bip32_index('Account Number:') or 0
-    elif ch != 'y':
+        acct = await ux_enter_bip32_index('Account Number:')
+
+    if (ch != 'y') or acct is None:
         return
 
     xfp = xfp2str(settings.get('xfp', 0))
     fname = 'unchained-%s.json' % xfp
 
-    await export_contents('Unchained', lambda: generate_unchained_export(account_num),
+    await export_contents('Unchained', lambda: generate_unchained_export(acct),
                           fname, is_json=True)
 
 

shared/address_explorer.py
@@ -115,7 +115,7 @@ class KeypathMenu(MenuSystem):
         val = item.arg or item.label
         assert val.endswith('/⋯')
         cpath = val[:-2]
-        nl = await ux_enter_bip32_index('%s/' % cpath, unlimited=True)
+        nl = await ux_enter_bip32_index('%s/' % cpath, unlimited=True, can_cancel=False)
         return KeypathMenu(cpath, nl, ranged=self.ranged, done_fn=self.done_fn)
 
 class PickAddrFmtMenu(MenuSystem):
@@ -241,11 +241,15 @@ class AddressListMenu(MenuSystem):
             self.goto_idx(axi)
 
     async def change_account(self, *a):
-        self.account_num = await ux_enter_bip32_index('Account Number:') or 0
+        acct = await ux_enter_bip32_index('Account Number:')
+        if acct is None: return
+        self.account_num = acct
         await self.render()
 
     async def change_start_idx(self, *a):
-        self.start = await ux_enter_bip32_index("Start index:", unlimited=True)
+        idx = await ux_enter_bip32_index("Start index:", unlimited=True)
+        if idx is None: return
+        self.start = idx
         await self.render()
 
     async def pick_single(self, _1, _2, item):

shared/auth.py
@@ -1663,8 +1663,7 @@ class TXExplorer:
                     start += self.n
             elif ch == "2":
                 max_v = self.max_items - 1
-                res = await ux_enter_number("Start Idx (0-%d):" % max_v, max_value=max_v,
-                                            can_cancel=True)
+                res = await ux_enter_number("Start Idx (0-%d):" % max_v, max_value=max_v)
                 if res is None: continue
                 start = res
             else:

shared/ccc.py
@@ -746,7 +746,7 @@ class SpendingPolicyMenu(MenuSystem):
         # Looks decent on both Q and Mk4...
         was = self.policy.get('mag', 0)
         val = await ux_enter_number('Transaction Max:', max_value=int(1e8),
-                                    can_cancel=True, value=(was or ''))
+                                    value=(was or ''))
 
         args = dict(mag=val)
         if (val is None) or (val == was):

shared/drv_entro.py
@@ -124,8 +124,7 @@ async def drv_entro_step2(_1, picked, _2, just_pick=False):
 
     msg = "Password Index?" if picked == 7 else "Index Number?"
     index = await ux_enter_bip32_index(msg, unlimited=settings.get("b85max", False))
-    if index is None:
-        return
+    if index is None: return
 
     dis.fullscreen("Working...")
     new_secret, width, s_mode, path = bip85_derive(picked, index)
@@ -292,7 +291,7 @@ async def password_entry(*args, **kwargs):
 
         while True:
             the_ux.pop()
-            index = await ux_enter_bip32_index("Password Index?", can_cancel=True)
+            index = await ux_enter_bip32_index("Password Index?")
             if index is None:
                 break
 

shared/msgsign.py
@@ -179,14 +179,16 @@ async def msg_sign_ux_get_subpath(addr_fmt):
     purpose = chains.af_to_bip44_purpose(addr_fmt)
     chain_n = chains.current_chain().b44_cointype
 
-    acct = await ux_enter_bip32_index('Account Number:') or 0
+    acct = await ux_enter_bip32_index('Account Number:')
+    if acct is None: return
 
     ch = await ux_show_story(title="Change?",
                              msg="Press (0) to use internal/change address,"
                                  " %s to use external/receive address." % OK, escape="0")
     change = 1 if ch == '0' else 0
 
-    idx = await ux_enter_bip32_index('Index Number:') or 0
+    idx = await ux_enter_bip32_index('Index Number:')
+    if idx is None: return
 
     return "m/%dh/%dh/%dh/%d/%d" % (purpose, chain_n, acct, change, idx)
 
@@ -408,6 +410,7 @@ async def ux_sign_msg(txt, approved_cb=None, kill_menu=True):
 
         text, af = item.arg
         subpath = await msg_sign_ux_get_subpath(af)
+        if subpath is None: return
 
         await approve_msg_sign(text, subpath, af, approved_cb=approved_cb,
                                kill_menu=kill_menu, only_printable=False)

shared/multisig.py
@@ -1566,7 +1566,8 @@ P2WSH:
         if ch != "y":
             return
 
-    acct = await ux_enter_bip32_index('Account Number:') or 0
+    acct = await ux_enter_bip32_index('Account Number:')
+    if acct is None: return
 
     def render(acct_num):
         sign_der = None
@@ -1779,7 +1780,8 @@ async def ondevice_multisig_create(mode='p2wsh', addr_fmt=AF_P2WSH, is_qr=False,
         secret, ccc_ms_count = for_ccc
         # Always include 2 keys from CCC: own master (key A) and key C
         # - force them to same derivation.
-        acct = await ux_enter_bip32_index('CCC Account Number:') or 0
+        acct = await ux_enter_bip32_index('CCC Account Number:')
+        if acct is None: return
 
         dis.fullscreen("Wait...")
         a = add_own_xpub(chain, acct, addr_fmt)                # master: key A
@@ -1804,7 +1806,8 @@ async def ondevice_multisig_create(mode='p2wsh', addr_fmt=AF_P2WSH, is_qr=False,
         ch = await ux_show_story("Add current Coldcard with above XFP ?",
                                  title="[%s]" % xfp2str(my_xfp))
         if ch == "y":
-            acct = await ux_enter_bip32_index('Account Number:') or 0
+            acct = await ux_enter_bip32_index('Account Number:')
+            if acct is None: return
             dis.fullscreen("Wait...")
             xpubs.append(add_own_xpub(chain, acct, addr_fmt))
             num_mine += 1
@@ -1819,10 +1822,8 @@ async def ondevice_multisig_create(mode='p2wsh', addr_fmt=AF_P2WSH, is_qr=False,
         M = 2
     else:
         # pick useful M value to start
-        M = await ux_enter_number("How many need to sign?(M)", N, can_cancel=True)
-        if not M:
-            await ux_dramatic_pause('Aborted.', 2)
-            return  # user cancel
+        M = await ux_enter_number("How many need to sign?(M)", N)
+        if M is None: return
 
     dis.fullscreen("Wait...")
 

shared/ux.py
@@ -349,7 +349,7 @@ async def show_qr_code(data, is_alnum=False, msg=None, **kw):
     o = QRDisplaySingle([data], is_alnum, msg=msg, **kw)
     await o.interact_bare()
 
-async def ux_enter_bip32_index(prompt, can_cancel=False, unlimited=False):
+async def ux_enter_bip32_index(prompt, can_cancel=True, unlimited=False):
     if unlimited:
         max_value = (2 ** 31) - 1  # we handle hardened
     else:

commit 75908965a7375d168c13be28995a24b53539da40

unreleased regression introduced in 300323f18d20d6e0a904f5f3bcc9d63d84d2f6a7

shared/actions.py
@@ -1100,6 +1100,7 @@ async def export_xpub(label, _2, item):
         if ch == 'x': return
         if ch == "2":
             slip132 = not slip132
+            continue
 
         if ch == '1':
             acct = await ux_enter_bip32_index('Account Number:')
@@ -1107,6 +1108,7 @@ async def export_xpub(label, _2, item):
             pth_split = path.split("/")
             pth_split[-1] = ("%dh" % acct)
             path = "/".join(pth_split)
+            continue
 
         # assume zero account if not picked
         path = path.format(acct=acct)
@@ -1148,7 +1150,7 @@ async def electrum_skeleton(a, b, item):
     if ch == '1':
         acct = await ux_enter_bip32_index('Account Number:')
 
-    if (ch != 'y') or acct is None:
+    if (ch not in '1y') or acct is None:
         return
 
     rv = [
@@ -1181,7 +1183,7 @@ async def ss_descriptor_skeleton(_0, _1, item):
         if ch == '1':
             acct = await ux_enter_bip32_index('Account Number:', unlimited=True)
 
-        if (ch != 'y') or acct is None:
+        if (ch not in '1y') or acct is None:
             return
 
     if int_ext is None:
@@ -1218,7 +1220,7 @@ async def key_expression_skeleton(_0, _1, item):
     if ch == '1':
         acct = await ux_enter_bip32_index('Account Number:', unlimited=True)
 
-    if (ch != 'y') or acct is None:
+    if (ch not in '1y') or acct is None:
         return
 
     # element on 2nd index is address format for signed exports
@@ -1292,7 +1294,7 @@ without ever connecting this Coldcard to a computer.\
     if ch == '1':
         acct = await ux_enter_bip32_index('Account Number:')
 
-    if (ch != 'y') or acct is None:
+    if (ch not in '1y') or acct is None:
         return
 
     # no choices to be made, just do it.
@@ -1314,7 +1316,7 @@ async def _generic_export(prompt, label, f_pattern):
     if ch == '1':
         acct = await ux_enter_bip32_index('Account Number:')
 
-    if (ch != 'y') or acct is None:
+    if (ch not in '1y') or acct is None:
         return
 
     await export_contents(label, lambda: generate_generic_export(acct),
@@ -1366,7 +1368,7 @@ This saves multisig XPUB information required to setup on the Unchained platform
     if ch == '1':
         acct = await ux_enter_bip32_index('Account Number:')
 
-    if (ch != 'y') or acct is None:
+    if (ch not in '1y') or acct is None:
         return
 
     xfp = xfp2str(settings.get('xfp', 0))