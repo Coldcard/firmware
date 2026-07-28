4ab07a0d81ce71c64f190b6c0c368da2a56c513e
scgbckbone <scgbckbone@proton.me>
2026-07-28 16:46:41 +0200

    v31 incompat

--- testing/test_sign.py
+++ testing/test_sign.py
@@ -778,7 +778,19 @@ def test_wrong_p2sh_p2wpkh(bitcoind, start_sign, end_sign, bitcoind_d_sim_watch,
     utxos = sim.listunspent()
     assert len(utxos) == 1
     conso_addr = sim.getnewaddress("", "legacy")
-    psbt_resp = sim.walletcreatefundedpsbt([], [{conso_addr: 1}], 0, {"fee_rate": 2, "change_type": "bech32"})
+    try:
+        # new in core v31 - default PSBT version is now 2
+        # old core version fails with "too many arguemnts"
+        # does not know the last one "psbt_version=0"
+        psbt_resp = sim.walletcreatefundedpsbt([], [{conso_addr: 1}], 0,
+                                               {"fee_rate": 2, "change_type": "bech32"},
+                                               True,  # BIP-32 paths
+                                               2,     # txn version
+                                               0)     # PSBT version
+    except:
+        psbt_resp = sim.walletcreatefundedpsbt([], [{conso_addr: 1}], 0,
+                                               {"fee_rate": 2, "change_type": "bech32"})
+
     psbt = psbt_resp.get("psbt")
     b4 = BasicPSBT().parse(base64.b64decode(psbt))
     t = CTransaction()
