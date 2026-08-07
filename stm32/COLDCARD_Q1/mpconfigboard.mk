d6dbd6f42aab5fc52ce2a586c4238bd3e6243bef
scgbckbone <scgbckbone@proton.me>
2026-08-07 09:57:58 +0200

    include schnorr/musig from libngu to save flash space; add adition macro NGU_STM32_EXTERNAL_RNG_GET; disable threads for unix port

--- stm32/COLDCARD_Q1/mpconfigboard.mk
+++ stm32/COLDCARD_Q1/mpconfigboard.mk
@@ -15,7 +15,10 @@ CFLAGS_EXTRA += -DMP_CONFIGFILE=\"boards/$(BOARD)/ckcc-port.h\"
 CFLAGS_EXTRA += -DCOLDCARD_DEBUG=$(DEBUG_BUILD)
 
 # NgU and uQR libraries
-NGU_NEEDS_CIFRA = 1
+NGU_NEEDS_CIFRA  = 1
+# Include libngu secp256k1 features required by Edge
+NGU_INCL_SCHNORR = 1
+NGU_INCL_MUSIG   = 1
 USER_C_MODULES = boards/$(BOARD)/c-modules
 
 # the bulk of the COLDCARD-specific code
