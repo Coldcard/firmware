ea083a6e3561a2bfc3f122d7dd844fbc6db8e70b
scgbckbone <scgbckbone@proton.me>
2026-08-20 15:08:45 +0200

    test improvements

--- testing/test_multisig.py
+++ testing/test_multisig.py
@@ -1321,6 +1321,7 @@ def make_myself_wallet(dev, set_bip39_pw, offer_ms_import, press_select, clear_m
             print(f"--- switch to another leg of MS: {idx} ---")
             xfp = set_bip39_pw(passwords[idx])
             if do_import and not no_import:
+                clear_ms()
                 offer_ms_import(config)
                 time.sleep(.1)
                 press_select()
