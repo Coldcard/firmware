d11a7cb7741e26b2eec099b552ab6cb626803cb9
scgbckbone <scgbckbone@proton.me>
2026-08-31 15:20:35 +0200

    test: cover seedless temporary backup restores

--- testing/seedless_tests.py
+++ testing/seedless_tests.py
@@ -1,11 +1,12 @@
 # (c) Copyright 2024 by Coinkite Inc. This file is covered by license found in COPYING-CC.
 #
-import pytest, pdb, time, random, os
+import pytest, pdb, time, random, os, shutil
 from charcodes import KEY_CANCEL, KEY_QR
-from core_fixtures import _pick_menu_item, _press_select
+from core_fixtures import _pick_menu_item, _press_select, _press_cancel, _word_menu_entry
 from core_fixtures import _need_keypress, _sim_exec, _cap_story
 from run_sim_tests import ColdcardSimulator, clean_sim_data
 from ckcc_protocol.client import ColdcardDevice
+from ckcc_protocol.protocol import CCProtocolPacker
 
 
 def test_status_bar_rewrite_after_restore_master():
@@ -67,5 +68,45 @@ def test_seedless_qr_import_bad_checksum():
 
         title, story = _cap_story(device)
         assert 'checksum fail' in story
+    finally:
+        sim.stop()
+
+
+def test_tmp_backup_restore_on_seedless():
+    clean_sim_data()
+    backup = os.path.realpath('./data/ckcc-backup.txt')
+    shutil.copy(backup, os.path.realpath('../unix/work/MicroSD/ckcc-backup.txt'))
+
+    sim = ColdcardSimulator(args=["--q1", "-l"])
+    sim.start(start_wait=3)
+    device = ColdcardDevice(is_simulator=True)
+    try:
+        _pick_menu_item(device, True, "Advanced/Tools")
+        _pick_menu_item(device, True, "Temporary Seed")
+        _need_keypress(device, "4")
+        _pick_menu_item(device, True, "Import Words")
+        _pick_menu_item(device, True, "12 Words")
+        _word_menu_entry(device, True, ["abandon"] * 11 + ["about"])
+        time.sleep(.5)
+        assert "New temporary master key is in effect now." in _cap_story(device)[1]
+        _press_select(device, True)
+
+        _pick_menu_item(device, True, "Advanced/Tools")
+        _pick_menu_item(device, True, "Danger Zone")
+        _pick_menu_item(device, True, "I Am Developer.")
+        _pick_menu_item(device, True, "Restore Bkup")
+        _pick_menu_item(device, True, "ckcc-backup.txt")
+        time.sleep(.2)
+        assert "load backup as temporary seed" in _cap_story(device)[1]
+        _press_cancel(device, True)
+        time.sleep(2.2)
+
+        with open(backup, "rb") as fd:
+            file_len, sha = device.upload_file(fd.read())
+        device.send_recv(CCProtocolPacker.restore_backup(file_len, sha, plaintext=True),
+                         timeout=None)
+        time.sleep(.2)
+        assert "Restore uploaded backup as a temporary seed?" in _cap_story(device)[1]
+        _press_cancel(device, True)
     finally:
         sim.stop()
